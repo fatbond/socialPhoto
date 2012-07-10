006c90b8adeeba5a271d858328d0a8a05a9e41e7
Le Quan <lequana2@gmail.com>
2012-07-10 10:19:38 +0700

    Add CustomTabbar lib

---
+++ socialPhoto/socialPhoto/Class/Controller/CustomTabBarController.h
@@ -0,0 +1,17 @@
+//
+//  Tab.h
+//  CustomTab
+//
+//  Created by Le Quan on 6/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomTabBarController : UITabBarController 
+
+- (void) changeBackgroundImage:(UIImage*)backgroundImage;
+- (void) addTabItemWithImage:(NSString*) normalImageName
+         andSelectedImage:(NSString*) selectedImageName;
+
+@end
