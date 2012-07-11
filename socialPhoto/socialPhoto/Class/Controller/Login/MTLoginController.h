2a064536d98f224eb24bed972079b6e3a8b4a114
Le Quan <lequana2@gmail.com>
2012-07-11 11:57:54 +0700

    add extended-NSString encoding class

--- socialPhoto/socialPhoto/Class/Controller/Login/MTLoginController.h
+++ socialPhoto/socialPhoto/Class/Controller/Login/MTLoginController.h
@@ -7,7 +7,23 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "ASIHTTPRequest.h"
+#import "ASIFormDataRequest.h"
+#import "SBJson.h"
+#import "NSString+MD5.h"
+#import "MTUserController.h"
 
-@interface MTLoginController : UIViewController
+@interface MTLoginController : UIViewController <UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate, ASIHTTPRequestDelegate>
+
++ (NSString*) userID;
++ (NSString*) deviceToken;
+
+@property (nonatomic, retain) NSArray *listData;
+
+@property (nonatomic, retain) IBOutlet UITableView *tableView;
+
+@property (nonatomic, retain) UITextField *textField;
+
++ (NSString*) getUserID;
 
 @end
---
+++ socialPhoto/socialPhoto/Class/Controller/Login/NSString+MD5.h
@@ -0,0 +1,15 @@
+//
+//  NSString.h
+//  SignUpApp
+//
+//  Created by Quan Le on 7/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NSString(MD5)
+
+- (NSString *)MD5;
+
+@end
